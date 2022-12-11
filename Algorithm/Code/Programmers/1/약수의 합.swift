4bee887110f28ae94e223568ec8c6cade2f7fb81
erun94510 <erun9414@icloud.com>
2022-12-11 16:42:59 +0900

    프로그래머스

--- Algorithm/Code/Programmers/1/약수의 합.swift
+++ Algorithm/Code/Programmers/1/약수의 합.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+//문제: 정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+//입출력 예 #1
+//12의 약수는 1, 2, 3, 4, 6, 12입니다. 이를 모두 더하면 28입니다.
+//입출력 예 #2
+//5의 약수는 1, 5입니다. 이를 모두 더하면 6입니다.
+
+func solution1(_ n:Int) -> Int {
+    return (1...n).filter { n % $0 == 0 }.reduce(0,+)
+}
+
+func solution2(_ n:Int) -> Int {
+    var result: Int = 0
+    if n != 0 {
+        for i in 1 ... n {
+            if n % i == 0 {
+                result += i
+            }
+        }
+    }
+    return result
+}
--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -7,139 +7,153 @@
 
 import Foundation
 
-
-func solution(_ s:String) -> Int {
+func solution(_ n:Int) -> Int {
     var result: Int = 0
-    var firstAndSecond: [String] = ["", ""]
-    var firstCount: Int = 0
-    var secondCount: Int = 0
-    
-    for i in s {
-        if firstAndSecond[0] == "" {
-            firstAndSecond[0] = String(i)
+    if n != 0 {
+        for i in 1 ... n {
+            if n % i == 0 {
+                result += i
+            }
         }
-        
-        if firstAndSecond[0] == String(i) {
-            firstCount += 1
-            print("first: \(i)")
-        } else {
-            secondCount += 1
-            firstAndSecond[1] = String(i)
-            print("second: \(i)")
-        }
-        
-        if firstCount == secondCount {
-            result += 1
-            print("result: \(result)")
-            firstCount = 0
-            secondCount = 0
-            firstAndSecond[0] = ""
-            firstAndSecond[1] = ""
-        }
- 
-    }
-    if firstAndSecond[0] != "" && firstAndSecond[1] == "" {
-        result += 1
     }
     return result
 }
 
-print(solution("abracadabra"))
+print(solution(12))
 
 
-
-
-func solution(_ n:Int) -> Int {
-    let t = type(of: sqrt(n))
-
-    return t == Int ? 1 : 2
-}
-
-var sol =  solution(144)
-print(sol)
-
-func solution(_ numbers:[Int], _ hand:String) -> String {
-
-    var result: String = ""
-
-    for i in numbers {
-        switch i {
-        case 1, 4, 7:
-            result += "L"
-        case 3, 6, 9:
-            result += "R"
-        case 2, 5, 8, 0:
-            if hand == "left" {
-                result += "L"
-            } else {
-                result += "R"
-            }
-        default:
-            result += ""
-        }
-    }
-
-    print(result)
-    return ""
-}
-
-var sol = solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2], "left")
-
-print(sol)
-
-func solution(_ my_string:String, _ letter:String) -> String {
-    return String(my_string.map { String($0!)! }.filter { $0 != letter }!)
-}
-
-var sol = solution("abcdef", "f")
-print(sol)
-
-
-
-
-
-
-
-
-
-
-
-func solution(_ quiz:[String]) -> [String] {
-
-    var result: [String] = []
-    var resultA: [[String]] = []
-
-    for _ in quiz {
-        resultA[] += quiz.split(separator: " ")
-        print(resultA)
-    }
-
-    return []
-}
-
-
-func solution(_ numlist:[Int], _ n:Int) -> [Int] {
-    var result: [Int] = []
-    var resultIndex: [Int : Int] = [:]
-    var index: Int = 0
-
-    for (index, number) in numlist.enumerated() {
-        resultIndex.append((index : number))
-        index += 1
-    }
-
-    for _ in result {
-
-    }
-    print(result)
-    print(resultIndex)
-
-    return []
-}
-
-
-var sol = solution([1, 2, 3, 4, 5, 6], 4)
-print(sol)
-
-var sol2 = solution([10000, 20, 36, 47, 40, 6, 10, 7000], 30)
-print(sol2)
+//func solution(_ s:String) -> Int {
+//    var result: Int = 0
+//    var firstAndSecond: [String] = ["", ""]
+//    var firstCount: Int = 0
+//    var secondCount: Int = 0
+//
+//    for i in s {
+//        if firstAndSecond[0] == "" {
+//            firstAndSecond[0] = String(i)
+//        }
+//
+//        if firstAndSecond[0] == String(i) {
+//            firstCount += 1
+//            print("first: \(i)")
+//        } else {
+//            secondCount += 1
+//            firstAndSecond[1] = String(i)
+//            print("second: \(i)")
+//        }
+//
+//        if firstCount == secondCount {
+//            result += 1
+//            print("result: \(result)")
+//            firstCount = 0
+//            secondCount = 0
+//            firstAndSecond[0] = ""
+//            firstAndSecond[1] = ""
+//        }
+//
+//    }
+//    if firstAndSecond[0] != "" && firstAndSecond[1] == "" {
+//        result += 1
+//    }
+//    return result
+//}
+//
+//print(solution("abracadabra"))
+//
+//
+//
+//
+//func solution(_ n:Int) -> Int {
+//    let t = type(of: sqrt(n))
+//
+//    return t == Int ? 1 : 2
+//}
+//
+//var sol =  solution(144)
+//print(sol)
+//
+//func solution(_ numbers:[Int], _ hand:String) -> String {
+//
+//    var result: String = ""
+//
+//    for i in numbers {
+//        switch i {
+//        case 1, 4, 7:
+//            result += "L"
+//        case 3, 6, 9:
+//            result += "R"
+//        case 2, 5, 8, 0:
+//            if hand == "left" {
+//                result += "L"
+//            } else {
+//                result += "R"
+//            }
+//        default:
+//            result += ""
+//        }
+//    }
+//
+//    print(result)
+//    return ""
+//}
+//
+//var sol = solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2], "left")
+//
+//print(sol)
+//
+//func solution(_ my_string:String, _ letter:String) -> String {
+//    return String(my_string.map { String($0!)! }.filter { $0 != letter }!)
+//}
+//
+//var sol = solution("abcdef", "f")
+//print(sol)
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//func solution(_ quiz:[String]) -> [String] {
+//
+//    var result: [String] = []
+//    var resultA: [[String]] = []
+//
+//    for _ in quiz {
+//        resultA[] += quiz.split(separator: " ")
+//        print(resultA)
+//    }
+//
+//    return []
+//}
+//
+//
+//func solution(_ numlist:[Int], _ n:Int) -> [Int] {
+//    var result: [Int] = []
+//    var resultIndex: [Int : Int] = [:]
+//    var index: Int = 0
+//
+//    for (index, number) in numlist.enumerated() {
+//        resultIndex.append((index : number))
+//        index += 1
+//    }
+//
+//    for _ in result {
+//
+//    }
+//    print(result)
+//    print(resultIndex)
+//
+//    return []
+//}
+//
+//
+//var sol = solution([1, 2, 3, 4, 5, 6], 4)
+//print(sol)
+//
+//var sol2 = solution([10000, 20, 36, 47, 40, 6, 10, 7000], 30)
+//print(sol2)
