ac06060a0e01f04212db47a4b2cf913f876cdc95
Donghoon Bae <65296382+erun94510@users.noreply.github.com>
2022-12-17 19:00:23 +0900

    프로그래머스

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -9,19 +9,19 @@ import Foundation
 
 
 
-//func solution(_ n:Int) -> Int {
-//    var result: Int = 0
-//    if n != 0 {
-//        for i in 1 ... n {
-//            if n % i == 0 {
-//                result += i
-//            }
-//        }
-//    }
-//    return result
-//}
-//
-//print(solution(12))
+func solution(_ n:Int) -> Int {
+   var result: Int = 0
+   if n != 0 {
+       for i in 1 ... n {
+           if n % i == 0 {
+               result += i
+           }
+       }
+   }
+   return result
+}
+
+print(solution(12))
 
 
 //func solution(_ s:String) -> Int {
